3e46715f3f9d9f33b3e3e681077ec388cd5ea0a7
Gabriel Babler <gjacobbabler@atlassian.com>
2024-09-25 22:28:40 -0300

    spring initializr

---
+++ src/main/java/com/gbabler/challenge_one/ChallengeOneApplication.java
@@ -0,0 +1,13 @@
+package com.gbabler.challenge_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeOneApplication.class, args);
+	}
+
+}
